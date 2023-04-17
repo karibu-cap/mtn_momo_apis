ee8b6ce9b7f957830685557319007561167d0429
Tadjaur <wk.taur@gmail.com>
2023-04-17 04:09:19 +0100

    Config jest to reset all mocks

--- jest.config.ts
+++ jest.config.ts
@@ -22,8 +22,13 @@ const jestConfig: Config = {
   // Module file extensions for importing
   moduleFileExtensions: ['ts', 'js'],
 
-  // Automatically reset mock state before every test
-  resetMocks: true,
+  // Automatically restore mock state before every test
+  restoreMocks: true,
+
+  // A list of paths to modules that run some code to configure or set up the testing environment.
+  setupFiles: [
+    './jest.setup.ts'
+  ],
 
   // The test environment that will be used for testing
   testEnvironment: 'node',
